6af51ff11635d189eb00af560aaccb6363fd0c1c
Ksusha <ksushalive@yandex.ru>
2022-11-21 19:04:14 +0300

    init6: start work to program6

---
+++ Example006/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = new Random().Next(1,10);
+int b = new Random().Next(1,10);
+int c = new Random().Next(1,10);
+int d = new Random().Next(1,10);
+int e = new Random().Next(1,10);
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.WriteLine($"max = {max}");
