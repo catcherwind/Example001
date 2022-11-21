76f3b803f2079c478c51e32bc975e35b1d7d60d2
Ksusha <ksushalive@yandex.ru>
2022-11-21 18:47:37 +0300

    init5: start work to program5

---
+++ Example005/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите имя");
+string name = Console.ReadLine();
+
+if (name.ToLower == "masha")
+{
+    Console.WriteLine($"Ура, это же Маша!!!");
+}
+else
+    Console.WriteLine($"Привет, {name}");
