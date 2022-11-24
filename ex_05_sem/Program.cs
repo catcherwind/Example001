34bb867aff9e90cb6218aaadaae6bf36aad0df91
Ksusha <ksushalive@yandex.ru>
2022-11-24 20:44:14 +0300

    init: start work to program 05

---
+++ ex_05_sem/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите первое число");
+int n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int n2 = int.Parse(Console.ReadLine());
+Console.WriteLine(n1 % n2 == 0 ? "Число кратно" : $"Число не кратно. Остаток от деления {n1 % n2}");
