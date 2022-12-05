70aa0039c2f86952ad30306a71732067fe9a86a0
Ksusha <ksushalive@yandex.ru>
2022-12-05 20:33:10 +0300

    init: z30

---
+++ ex_30_sem/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] FillingArray(int n){
+    Random rnd = new Random();
+    int[] arr = new int[n];
+    for(int i = 0; i < n; i++){
+        arr[i] = rnd.Next(0,2);
+    }
+    return arr;
+}
+
+int[] arr = FillingArray(8);
+Console.WriteLine(String.Join(" ", arr));
